809d6fc8987f9ae4140a3941cc59b92ade7c4f3e
jacobm2000 <maurerja2000@gmail.com>
2022-10-04 14:25:04 -0400

    added flask funcitonaity
    Integrated the stock price webscraper into a flask project

---
+++ flask-main.py
@@ -0,0 +1,17 @@
+from main import *
+from flask import Flask, render_template,request,flash
+
+app=Flask(__name__)
+
+@app.route("/home",methods=["PoST","GET"])
+
+def home() :
+    if request.method=="POST":
+        output=(getPrice( str(request.form["t"])))
+        return render_template("home.html", output=output)
+    else:
+        return render_template("home.html")
+
+
+if __name__== "__main__":
+    app.run(debug=False)
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-sym=input("enter a symbol name: ")
-url = 'https://www.marketwatch.com/investing/stock/'+str(sym)
-page = requests.get(url)
+
+def getPrice(x):
+    sym=str(x)
+    url = 'https://www.marketwatch.com/investing/stock/'+str(sym)
+    page = requests.get(url)
+    
+    
+    try:
+        rstr=""
+        soup = BeautifulSoup(page.text, 'html.parser')
+    
+        company = soup.find('h1', {'class':'company__name'}).text
+        price = soup.find('h2',{'class':'intraday__price'})
+        #used to find the price from within the h2
+        s=BeautifulSoup(price.text,'html.parser')
+        s.find('bg-quote')
+        price=s
+        rstr+=("company Name: " +company)
+        #Get rid of the dollar sign and the extra lines by replacing and spliting
+        price=str(price).replace("$","").splitlines()[2]
+        rstr+=("\nPrice: " +price)
+        return rstr
+    except:
+        return("Could not find that symbol")
 
 
-try:
-    soup = BeautifulSoup(page.text, 'html.parser')
 
-    company = soup.find('h1', {'class':'company__name'}).text
-    price = soup.find('h2',{'class':'intraday__price'})
-    #used to find the price from within the h2
-    s=BeautifulSoup(price.text,'html.parser')
-    s.find('bg-quote')
-    price=s
-    print("company Name: " +company)
-    #Get rid of the dollar sign and the extra lines by replacing and spliting
-    price=str(price).replace("$","").splitlines()[2]
-    print("price: " +price)
-except:
-    print("Could not find that symbol")
 
+print(getPrice("aapl"))
